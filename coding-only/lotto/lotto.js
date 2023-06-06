bf5ddf9c1100abe93aa73771b90845619b662472
Kai Weluda <kaiweluda@gmail.com>
2023-06-06 12:45:50 +0200

    feat: adds the lotto kata

--- coding-only/lotto/lotto.js
+++ coding-only/lotto/lotto.js
@@ -1,3 +1,23 @@
 export function randomLottoTicket() {
-  // Add your code here
+  const outputArray = [];
+
+  function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+  }
+
+  function uniqueNumber() {
+    const newNumber = getRandomIntInclusive(1, 49);
+    if (outputArray.includes(newNumber)) {
+      uniqueNumber();
+    } else {
+      return newNumber;
+    }
+  }
+
+  for (let i = 0; i < 6; i++) {
+    outputArray.push(uniqueNumber());
+  }
+  return outputArray;
 }
